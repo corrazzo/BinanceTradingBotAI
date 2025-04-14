4fd0c61fa5686a4517d1085c5a983dc1091859ae
corrazzo <officialcreativ@gmail.com>
2025-04-14 07:27:36 +0200

    Initial commit

---
+++ app/src/main/java/com/binancebot/MainActivity.java
@@ -0,0 +1,14 @@
+
+package com.binancebot;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Tu można podpiąć logikę predykcji, analizy i wyświetlania wykresów
+    }
+}
